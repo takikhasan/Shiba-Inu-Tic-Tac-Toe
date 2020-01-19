f1dc0a57a6c6db21b86337be93a3da46c4d853be
Takik Hasan <hasantakik@gmail.com>
2020-01-19 23:57:46 +0600

    Updates

---
+++ precache-manifest.78aa6081d9fea39f3e5214ed89643bb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aea55f5f3633c1e475634378937f8c5e",
+    "url": "/Shiba-Inu-Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "d4a435e551bf1351e7aa",
+    "url": "/Shiba-Inu-Tic-Tac-Toe/static/css/main.04a84f23.chunk.css"
+  },
+  {
+    "revision": "aae2a4fc90b9ba91104c",
+    "url": "/Shiba-Inu-Tic-Tac-Toe/static/js/2.bebc24d7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Shiba-Inu-Tic-Tac-Toe/static/js/2.bebc24d7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d4a435e551bf1351e7aa",
+    "url": "/Shiba-Inu-Tic-Tac-Toe/static/js/main.bb7b0efd.chunk.js"
+  },
+  {
+    "revision": "8b47b2476ad767c89816",
+    "url": "/Shiba-Inu-Tic-Tac-Toe/static/js/runtime-main.5ce1f873.js"
+  }
+]);
